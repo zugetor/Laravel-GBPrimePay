28be25c9ab74af34ce2fafdf635459db2e98e615
ZuGeTor <14806377+zugetor@users.noreply.github.com>
2022-05-16 23:05:01 +0700

    Fix config not publish

---
+++ config/gbprimepay.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'url' => env('GB_URL', 'https://api.gbprimepay.com'),
+    'token' => env('GB_TOKEN', null),
+    'backgroundUrl' => env('GB_BG_URL', ''),
+    'public_key' => env('GB_PUBLIC_KEY', null),
+    'secret_key' => env('GB_SECRET_KEY', null),
+];
--- config/payments.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'url' => env('URL', 'https://api.gbprimepay.com'),
-    'token' => env('TOKEN', null),
-    'backgroundUrl' => env('BG_URL', ''),
-    'public_key' => env('PUBLIC_KEY', null),
-    'secret_key' => env('SECRET_KEY', null),
-];
